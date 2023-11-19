cfac770de2746eb6e81201e4a5005aa05d00f280
courage <asphyxia123@protonmail.com>
2023-11-19 13:05:38 +0100

    tree took a fat shit and overflew the stack

---
+++ target/debug/build/glutin-55c00aa8aa19e2a5/build_script_build-55c00aa8aa19e2a5.d
@@ -0,0 +1,5 @@
+/home/courage/Documents/process-viewer/target/debug/build/glutin-55c00aa8aa19e2a5/build_script_build-55c00aa8aa19e2a5: /home/courage/.cargo/registry/src/index.crates.io-6f17d22bba15001f/glutin-0.30.10/build.rs
+
+/home/courage/Documents/process-viewer/target/debug/build/glutin-55c00aa8aa19e2a5/build_script_build-55c00aa8aa19e2a5.d: /home/courage/.cargo/registry/src/index.crates.io-6f17d22bba15001f/glutin-0.30.10/build.rs
+
+/home/courage/.cargo/registry/src/index.crates.io-6f17d22bba15001f/glutin-0.30.10/build.rs:
